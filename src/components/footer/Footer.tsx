bb22662b2266daf22c36eecd9b5380a6439d7869
Daniel Lima <dsl.daniel.santos.lima@gmail.com>
2025-01-30 09:54:56 -0300

    atualizacao da footer

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -2,7 +2,7 @@ import { FaFacebook, FaGithub, FaInstagram, FaLinkedin } from "react-icons/fa";
 
 export default function Footer() {
   return (
-    <footer className="bg-charcoal text-white py-4">
+    <footer className="bg-charcoal text-white py-4 rounded-t-xl">
       <div className="container mx-auto px-6 flex flex-col md:flex-row justify-between items-center">
         {/* Nome da Empresa */}
         <div className="text-lg font-semibold mb-4 md:mb-0">
@@ -20,16 +20,16 @@ export default function Footer() {
 
         {/* Redes Sociais */}
         <div className="flex space-x-4">
-          <a href="https://www.facebook.com/generationbrasil" className="text-white hover:text-lime-green">
+          <a href="https://www.facebook.com/generationbrasil" target="_blank" className="text-white hover:text-lime-green">
             <FaFacebook size={24} />
           </a>
-          <a href="https://github.com/projetoIntegrador-aplicacaoDelivery/FrontEnd" className="text-white hover:text-lime-green">
+          <a href="https://github.com/projetoIntegrador-aplicacaoDelivery/FrontEnd" target="_blank" className="text-white hover:text-lime-green">
             <FaGithub size={24} />
           </a>
-          <a href="https://www.instagram.com/generationbrasil" className="text-white hover:text-lime-green">
+          <a href="https://www.instagram.com/generationbrasil" target="_blank" className="text-white hover:text-lime-green">
             <FaInstagram size={24} />
           </a>
-          <a href="https://www.linkedin.com/school/generationbrasil" className="text-white hover:text-lime-green">
+          <a href="https://www.linkedin.com/school/generationbrasil" target="_blank" className="text-white hover:text-lime-green">
             <FaLinkedin size={24} />
           </a>
         </div>
